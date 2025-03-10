c0cee908c61081fec6d9507bc80af8222d5b7907
Eteran07 <edgar.teran@correo.unimet.edu.ve>
2025-03-10 17:43:16 -0400

    Se crearon los componentes a utilizar y se configuro el firebase(aun no se ha pegado su api),asimismo se instalaron los componentes necesarios para el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Login from './components/Login';
+import Home from './components/Home';
+import Destinations from './components/Destinations';
+import Info from './components/Info';
+import Contact from './components/Contact';
+import Booking from './components/Booking';
 
-export default function App() {
+const App = () => {
   return (
-    <> 
-    
-    <div className="text-[25px] text-primary">Hola Mundo</div>
-    <div className="text-[25px] text-primary">Hola Mundo</div>
-    <div className="text-[25px] text-primary">Hola Mundo</div>
-    <div className="text-[25px] text-primary">Hola Mundo</div>
-    <div className="text-[25px] text-secondary">Hola Mundo</div>
-     </>
-    
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booking/:destinationId" element={<Booking />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
+export default App;
