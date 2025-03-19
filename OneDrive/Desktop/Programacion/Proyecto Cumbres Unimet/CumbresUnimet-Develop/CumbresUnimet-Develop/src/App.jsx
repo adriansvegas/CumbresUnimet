05b0d11d6d5500ff8224e4c5010c8c8caaf59c9a
Samueleff <samuel.fernandes+1@correo.unimet.edu.ve>
2025-03-19 15:29:10 -0400

    Actualización en el Disenio

---
+++ OneDrive/Desktop/Programacion/Proyecto Cumbres Unimet/CumbresUnimet-Develop/CumbresUnimet-Develop/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Login from './components/Login';
+import Home from './components/Home';
+import Destinations from './components/Destinations';
+import Info from './components/Info';
+import Contact from './components/Contact';
+import Booking from './components/Booking';
+import Register from './components/Register';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/booking/:destinationId" element={<Booking />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
